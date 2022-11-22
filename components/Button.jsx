08bf55d8180147b34ac6d7459fd03ae9c2741c44
olayway <ola@yway.pl>
2022-11-22 19:23:13 +0100

    import Button incl Next Link

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,9 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 export function Button(props) {
     return (
         <div style={{ padding: 10, backgroundColor: '#333', color: '#fff', display: 'inline-block', borderRadius: 4 }} onClick={() => alert('Hi')}>
-            {props.title}
+            <Link
+                href="#"
+                className="text-blue-700 hover:text-blue-900"
+                legacyBehavior>
+                {props.title}
+            </Link>
         </div>
     )
 }
